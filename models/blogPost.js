821e759ef16406ffd33921abf6d0726fcf6d7d39
dmod129 <derek.modugno@gmail.com>
2023-11-12 19:05:07 -0600

    created blogPost model

--- models/blogPost.js
+++ models/blogPost.js
@@ -1,3 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
+
+class BlogPost extends Model {};
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogPost',
+  }
+);
+
+module.export = BlogPost;
