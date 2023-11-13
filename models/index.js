0729cb4d6541644567ea0c6f0fa27e31bb728941
dmod129 <derek.modugno@gmail.com>
2023-11-12 19:47:57 -0600

    created index model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
-const Post = require('./post');
-const User = require('./user');
-const Comment = require('./comment');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User
+User.hasMany(Post, {
+    foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'comment_id',
+} );
+
+// Post
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'comment_id',
+});
+
+// Comment
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
